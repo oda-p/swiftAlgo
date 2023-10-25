50fe5d3e434edc5fc77e30a3ff562c3378ff2723
junstone1995 <boris0730@gmail.com>
2023-10-25 22:01:18 +0900

    백준 24262: 알고리즘 수행시간 1

---
+++ algo/backjoon/24262.swift
@@ -0,0 +1,16 @@
+//
+//  main.swift
+//  algo
+//
+//  Created by lee on 10/25/23.
+//
+
+// MenOfPassion(A[], n) {
+//    i = ⌊n / 2⌋;
+//    return A[i]; # 코드1
+// }
+
+import Foundation
+
+print(1)
+print(0)
