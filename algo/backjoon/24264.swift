c0c1ba6ff91baff1c2035f4b6ce6c23368c5bb47
junstone1995 <boris0730@gmail.com>
2023-10-26 20:18:46 +0900

    백준 24264: 알고리즘 수행시간 3

---
+++ algo/backjoon/24264.swift
@@ -0,0 +1,21 @@
+//
+//  main.swift
+//  algo
+//
+//  Created by lee on 10/26/23.
+//
+
+// MenOfPassion(A[], n) {
+//    sum <- 0; // 1
+//    for i <- 1 to n // n
+//        for j <- 1 to n // n
+//            sum <- sum + A[i] × A[j]; # 코드1 // 1 + n x n
+//    return sum;
+// }
+import Foundation
+
+var num = Int(readLine()!)!
+
+
+print(num * num)
+print(2)
