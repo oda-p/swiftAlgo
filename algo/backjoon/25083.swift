e6909c14b04f2490dc6fcaffe9eb575aab834109
junstone1995 <boris0730@gmail.com>
2023-11-17 20:47:57 +0900

    25083

---
+++ algo/backjoon/25083.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  algo
+//
+//  Created by lee on 11/17/23.
+//
+
+import Foundation
+
+print("""
+         ,r'\"7
+r`-_   ,'  ,/
+ \\. ". L_r'
+   `~\\/
+      |
+      |
+""")
