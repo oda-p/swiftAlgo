979912bdc2706ca328f9d252ca169ce29bc48441
junstone1995 <boris0730@gmail.com>
2024-01-03 21:56:27 +0900

    1431: 시리얼

---
+++ algo/backjoon/1431.swift
@@ -0,0 +1,38 @@
+//
+//  main.swift
+//  algo
+//
+//  Created by lee on 1/3/24.
+//
+
+import Foundation
+
+var serials = [String]()
+for _ in 0 ..< Int(readLine()!)! {
+    serials.append(readLine()!)
+}
+
+func getSum(_ s: String) -> Int {
+    let str = Array(s).map { String($0) }
+    var sum = 0
+    for char in str {
+        if Int(char) != nil {
+            sum += Int(char)!
+        }
+    }
+    return sum
+}
+
+serials.sort {
+    if $0.count == $1.count {
+        if getSum($0) == getSum($1) {
+            return $0 < $1
+        } else {
+            return getSum($0) < getSum($1)
+        }
+    } else {
+        return $0.count < $1.count
+    }
+}
+
+serials.forEach { print($0) }
